523bb652545d7374944f1243ab05e3ce8f663849
Sergey Babich <sergei.babich2022@yandex.by>
2023-03-05 01:29:01 +0300

    Add hw09 homeworks. Refactor

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -42,7 +42,7 @@ function Clock() {
 
     const formatterDateDay = new Intl.DateTimeFormat('en-US', {day: '2-digit'}).format(date)
     const formatterDateMonth = new Intl.DateTimeFormat('en-US', {month: '2-digit'}).format(date)
-    const formatterDateYear = new Intl.DateTimeFormat('en-US', {year: '2-digit'}).format(date)
+    const formatterDateYear = new Intl.DateTimeFormat('en-US', {year: 'numeric'}).format(date)
     const formatterDate = `${formatterDateDay}.${formatterDateMonth}.${formatterDateYear}`
 
     const formatterDay = new Intl.DateTimeFormat('en-US', {weekday: 'long'});
