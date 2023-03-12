58c0404a8f8ca1b0bbb3a4ddd15c5a44b8b1c3b4
Sergey Babich <sergei.babich2022@yandex.by>
2023-03-12 18:52:29 +0300

    Add hw10 homeworks.

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,6 @@
 import s from './Loader.module.css'
+import preloader from "./preloader.svg"
 
-export const Loader = () => <div className={s.loader}/>
+
+// export const Loader = () => <div className={s.loader}/>
+export const Loader = () => <img src={preloader} alt="image" className={s.loader}/>
--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,10 +1,20 @@
+import {AppStoreType} from './store';
+
+
+export type LoadingType = {
+    isLoading: boolean
+}
+
 const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: LoadingActionType): LoadingType => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
+        case 'CHANGE_LOADING': {
+            return {...state, isLoading: action.isLoading}
+        }
 
         default:
             return state
